dffae5138608aa9b87827c257f7b62b680d6cc0d
hyunwook <hyunwook1994@naver.com>
2017-03-31 17:06:33 +0900

    로그인체크완료

--- src/controller/LoginCheck.java
+++ src/controller/LoginCheck.java
@@ -8,6 +8,7 @@ import javax.servlet.ServletException;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
+import javax.servlet.http.HttpSession;
 
 import model.domain.UsrDTO;
 
@@ -15,12 +16,19 @@ public class LoginCheck extends HttpServlet {
    protected void service(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
       String usrId = request.getParameter("id");
       String usrPw = request.getParameter("pw");
-      
       try {
          UsrDTO d = UsrDAO.selectOne(usrId);
          //usr 정보가 일치한다면
          if(d.getUsrId().equals(usrId) && d.getUsrPw().equals(usrPw)) {
-            response.sendRedirect("logincomplete?usrId=" + usrId);
+          //세션 생성: client당 1씩 생성, 관리는 서버가 함
+            HttpSession session = request.getSession();
+            
+            //생성된 세션에 데이터 저장: server 메모리에 저장
+            session.setAttribute("usrId", usrId);
+            
+            System.out.println("usrId: " + usrId);
+            //메인페이지로 이동해야함!!!
+            response.sendRedirect("main.html");
          }
          //usr 정보가 일치하지 않는다면
          else {
